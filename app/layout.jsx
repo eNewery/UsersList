1abcf0d5d90d88eaa0ed0a2ddcb1be6edd788347
Ulises <robdiaz247@gmail.com>
2023-07-14 20:03:17 -0300

    small changes in the userCreate page.

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import Navigation from "../components/navigation"
 import './globals.css'
 import { Inter } from 'next/font/google'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
--- app/userCreate/page.jsx
+++ app/userCreate/page.jsx
@@ -1,9 +1,15 @@
 "use client"
+
 import React from 'react'
 import { useRouter } from 'next/navigation'
 const userCreate = () => {
+
+
+
     const router = useRouter()
     function createUser(){
+      const db = getFirestore()
+      const userId = 1; 
       const firstName = document.querySelector(".firstName")
       const lastName = document.querySelector(".lastName")
       const email = document.querySelector(".email")
@@ -18,25 +24,24 @@ const user = {
   passwordOb: password.value,
   repeatPasswordOb: repeatPassword.value
 }
-console.log(user)
 router.push("/userCreated")
     }
   return (
     <div className='userCreateContainer'>
         <div>
-        <h1>Create User</h1>
-        <p>Already have an account? <button onClick={() => router.push("/userLogin")} className='userCreateLogin'>Log-in</button></p>
+        <h1 className='createUserTitle'>Create User</h1>
         </div>
         <form className="createUserForm">
             <div>
         <input className='firstName' required type="text" placeholder='First Name'/>
         <input className='lastName' required type="text" placeholder='Last Name'/>
             </div>
-        <input className='email' required type="text" placeholder='E-Mail'/>
-        <input className='username' required type="text" placeholder='Username'/>
-        <input className='password' required type="password" placeholder='Password'/>
-        <input className='repeatPassword' required type="password" placeholder='Repeat Password'/>
+        <input className='email inputForm' required type="text" placeholder='E-Mail'/>
+        <input className='username inputForm' required type="text" placeholder='Username'/>
+        <input className='password inputForm' required type="password" placeholder='Password'/>
+        <input className='repeatPassword inputForm' required type="password" placeholder='Repeat Password'/>
         </form>
+        <p>Already have an account? <button onClick={() => router.push("/userLogin")} className='userCreateLogin'>Log-in</button></p>
         <button onClick={createUser} className='userCreateBtn'>Create User</button>
     </div>
   )
